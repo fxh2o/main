f4ae91e053814b33a18ea60462dc940699aa1403
FXh2o <ajsmodapk@gmail.com>
2026-09-13 00:12:10 +0600

    Adjust report popup text layout

--- notifications.js
+++ notifications.js
@@ -143,8 +143,8 @@
         <svg viewBox="0 0 24 24"><path d="M21.7 3.3 18.6 20c-.2 1.2-.9 1.5-1.8.9l-5-3.7-2.4 2.3c-.3.3-.5.5-1 .5l.4-5.1 9.3-8.4c.4-.4-.1-.6-.6-.2L6 13.7 1.1 12.2c-1.1-.3-1.1-1 .2-1.5L20.4 3c.9-.3 1.7.2 1.3.3Z"/></svg>
       </div>
       <div class="anipastaReportContent">
-        <div class="anipastaReportText">Report an error or request content</div>
-        <div class="anipastaReportHint">Contact us on Telegram</div>
+        <div class="anipastaReportText">Report an error or request</div>
+        <div class="anipastaReportHint">content</div>
       </div>
     `;
     document.body.appendChild(popup);
